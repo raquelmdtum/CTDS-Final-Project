8db499c2d9cf6bbc5c1184e284af1543da524c1f
Ana Marija <ana-marija.pavicic@fer.hr>
2024-12-05 21:47:20 +0100

    Remove import utility from imports

--- imports.py
+++ imports.py
@@ -6,7 +6,6 @@ import numpy as np
 import pandas as pd
 import seaborn as sns
 from tqdm import tqdm
-from utility import *
 import plotly.io as pio
 import tensorflow as tf
 import plotly.express as px
